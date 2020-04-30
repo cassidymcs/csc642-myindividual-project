c87f8f6a0693e89e3466703e421cdf9a8e536bc9
cassidymcs <cassidymcskimming@gmail.com>
2020-04-30 00:32:58 -0700

    pushed backend

---
+++ mybackend/routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'pepper12',
+  database: 'mydatabase',
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connection successful!, id: ' + connection.threadId);
+});
+
+
+/* GET users listing. */
+router.post('/', function(req, res, next) {
+  var username = req.body.username;
+  var password = req.body.password;
+  connection.query("SELECT * FROM user WHERE username = ? AND password = ?", [username, password], function(err, row, fields){
+    if (err) console.log(err);
+
+    if(row.length > 0){
+      res.send({'success': true, 'message': row[0].username});
+    } else {
+      res.send({'success': false, 'message': 'User not found, please try again'});
+    }
+  });
+
+});
+
+module.exports = router;
